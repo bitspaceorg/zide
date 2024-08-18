b131c9c51373c03b7ad6807e2371883777fb3f32
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-08-18 17:13:47 +0530

    feat: selectable toolbar ✨🍉

--- include/app.h
+++ include/app.h
@@ -2,13 +2,13 @@
 #include <vector>
 
 enum SelectedTool { SELECTED_PENCIL, SELECTED_ERASER, SELECTED_BUCKET };
-struct ToolBar {
+struct ToolbarState {
   SelectedTool selected_tool = SELECTED_PENCIL;
   int pencil_size = 10;
   int eraser_size = 10;
 };
 
-struct Timeline {
+struct TimelineState {
   int fps = 24;
   int total_frames = 1;
   int current_seekhead = 0;
@@ -18,7 +18,7 @@ struct Color {
   std::string name;
   float r, g, b, a;
 };
-struct ColorSwatch {
+struct ColorSwatchState {
   std::vector<Color> pallet{
       Color{.name = "black", .r = 0.0f, .g = 0.0f, .b = 0.0f, .a = 1.0f},
       Color{.name = "white", .r = 0.0f, .g = 0.0f, .b = 0.0f, .a = 1.0f},
@@ -31,15 +31,15 @@ struct AppState {
   bool is_toolbar_visible = true;
   bool is_colorswatch_visible = true;
 
-  ToolBar toolbar_state;
-  Timeline timeline;
-  ColorSwatch color_swatch;
+  ToolbarState toolbar_state;
+  TimelineState timeline;
+  ColorSwatchState color_swatch;
 };
 extern AppState app_state;
 
 void run_application();
 void initialize_application();
-static void render_toolbar();
+static void render_toolbar(ToolbarState *toolbar_state);
 static void render_main_menu_bar();
 static void render_timeline();
 static void render_color_swatch();
